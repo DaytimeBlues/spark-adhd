b188dcb80c361f2358988b54369c547ba01f5671
DaytimeBlues <194465096+DaytimeBlues@users.noreply.github.com>
2026-02-27 11:28:16 +1000

    chore: tighten test and lint guardrails

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/react-native/extend-expect";
+
+afterEach(() => {
+  jest.clearAllTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,17 @@ module.exports = {
   testPathIgnorePatterns: [
     "/node_modules/",
     "/detox/",
-    "/android.e2e.test.ts/",
+    "android\\.e2e\\.test\\.ts$",
   ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup.ts"],
 };
